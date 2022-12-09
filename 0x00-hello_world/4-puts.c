4fcd0da0368fde6e0fcd7cc80381520a106db55e
Htiag <joefaithokutepa0@gmail.com>
2022-12-09 11:01:58 -0800

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include <stdio.h>                                                              /*                                                                               * main - Entry                                                           
- * 
- * * Return: Always 0                                                         
-*/                                                                              int main(void)
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-	        puts("\"Programming is like building a multi lingual puzzle);
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
